f40575e53f87743903589c5bb124cbf03134547b
Mi6u3l <miguelbragagomes@gmail.com>
2019-01-27 15:54:49 +0000

    refactors login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { DropdownButton, MenuItem } from "react-bootstrap";
 import { setAuthedUser } from "../actions/authedUser";
 
-class Login extends Component {
-  handleSelect = authedId => {
-    const { dispatch } = this.props;
+const Login = ({ logIn, users }) => (
+  <div className="text-center">
+    <h3>Please Login</h3>
+    <DropdownButton
+      title="Select user"
+      id="usersList"
+      onSelect={authedId => logIn(authedId)}
+    >
+      {users.map(user => (
+        <MenuItem eventKey={user.id} key={user.id}>
+          {user.name}
+        </MenuItem>
+      ))}
+    </DropdownButton>
+  </div>
+);
 
-    dispatch(setAuthedUser(authedId));
-  };
-  render() {
-    return (
-      <div className="text-center">
-        <h3>Please Login</h3>
-        <DropdownButton
-          title="Select user"
-          id="usersList"
-          onSelect={this.handleSelect}
-        >
-          {this.props.users.map(user => (
-            <MenuItem eventKey={user.id} key={user.id}>
-              {user.name}
-            </MenuItem>
-          ))}
-        </DropdownButton>
-      </div>
-    );
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  logIn: authedId => dispatch(setAuthedUser(authedId))
+});
 
-function mapStateToProps({ users }) {
-  return {
-    users: Object.keys(users).map(userId => users[userId])
-  };
-}
+const mapStateToProps = ({ users }) => ({
+  users: Object.keys(users).map(userId => users[userId])
+});
 
-export default connect(mapStateToProps)(Login);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
